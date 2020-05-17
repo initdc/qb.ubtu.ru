ef44caedf406507bb4d4a5d7f35497f0e758fac5
apple <initd@outlook.com>
2020-05-18 01:08:07 +0800

    half life

--- main.go
+++ main.go
@@ -1,29 +1,34 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
-
+	_ "fmt"
 	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
+	"io/ioutil"
+	"net/http"
 )
 
-func main() {
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		log.Fatal("$PORT must be set")
+func check(e error) {
+	if e != nil {
+		panic(e)
 	}
+}
+
+func main() {
+	dat, _ := ioutil.ReadFile("sh/1key-docker-compose-ubuntu.sh")
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+	router.LoadHTMLGlob("templates/index.html")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"sh": string(dat),
+		})
 	})
 
-	router.Run(":" + port)
+	router.HEAD("/", func(d *gin.Context) {
+		d.File("sh/1key-docker-compose-ubuntu.sh")
+	})
+
+	router.Run(":80")
 }
